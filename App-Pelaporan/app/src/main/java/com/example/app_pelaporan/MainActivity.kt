62e963b30ea2af20530e158293e39ba8e5d70669
MiftahulH23 <miftahul21si@mahasiswa.pcr.ac.id>
2023-12-10 11:43:16 +0700

    create page laporan and saran

--- App-Pelaporan/app/src/main/java/com/example/app_pelaporan/MainActivity.kt
+++ App-Pelaporan/app/src/main/java/com/example/app_pelaporan/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.app_pelaporan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import androidx.fragment.app.Fragment
+import com.example.app_pelaporan.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        changeFragment(BerandaFragment())
+
+        binding.navbar.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.tab_home -> changeFragment(BerandaFragment())
+                R.id.tab_saran -> changeFragment(SaranFragment())
+                R.id.tab_laporan -> changeFragment(LaporanFragment())
+                else -> {}
+            }
+            true
+        }
     }
+
+    private fun changeFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(R.id.container, fragment)
+        fragmentTransaction.commit()
+    }
+
 }
